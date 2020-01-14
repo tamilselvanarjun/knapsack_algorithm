754599999b2c818e97e4ee064683d6194ebe2e0a
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2020-01-15 01:02:00 +0530

    creating test cases

--- knapsack/test.py
+++ knapsack/test.py
@@ -23,26 +23,5 @@ class TestKnapsack(unittest.TestCase):
         result = knapsack(values, weights, capacity)
         self.assertIsNone(result)
 
-    def test_invalid_input_zero_capacity(self):
-        values = [60, 100, 120]
-        weights = [10, 20, 30]
-        capacity = 0
-        result = knapsack(values, weights, capacity)
-        self.assertEqual(result, 0)  # Knapsack capacity is zero, so result should be zero
-
-    def test_invalid_input_zero_weight(self):
-        values = [60, 100, 120]
-        weights = [0, 0, 0]
-        capacity = 50
-        result = knapsack(values, weights, capacity)
-        self.assertEqual(result, 0)  # All items have zero weight, so result should be zero
-
-    def test_valid_input_duplicate_items(self):
-        values = [60, 100, 120, 60]  # Duplicate item value
-        weights = [10, 20, 30, 10]  # Duplicate item weight
-        capacity = 50
-        result = knapsack(values, weights, capacity)
-        self.assertEqual(result, 220)  # Duplicate items should not affect the result
-
 if __name__ == '__main__':
     unittest.main()
