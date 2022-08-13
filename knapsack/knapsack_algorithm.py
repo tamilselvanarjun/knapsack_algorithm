2a33a0e212cfa2ac3a6087930ebf8c15deb37b61
andersonmason <wzqeqpnz@hotmail.com>
2022-08-13 10:18:20 +0500

    Fix Formatting issues

--- knapsack/knapsack_algorithm.py
+++ knapsack/knapsack_algorithm.py
@@ -14,10 +14,10 @@ def knapsack(values, weights, capacity):
         for i in range(1, n + 1):
             for w in range(1, capacity + 1):
                 if weights[i-1] <= w:
-                    
+                    # If the current item can fit in the knapsack, choose the maximum between taking the item and not taking it
                     dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]] + values[i-1])
                 else:
-                    # If the current item cannot fit, do not take it.
+                    
                     dp[i][w] = dp[i-1][w]
 
         # The answer to the problem is in dp[n][capacity], which represents considering all items with the full capacity
